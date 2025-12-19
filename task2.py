1ef7abc3e9005a34cdfebe9f092f9c1f8b41ab7c
kemarirucker850-afk <kemari.rucker850@students.cmcss.net>
2025-12-19 08:53:59 -0600

    Update task2.py

--- task2.py
+++ task2.py
@@ -1,1 +1,16 @@
 
+# Week 1: Chores and first purchase
+# dishes += $4
+# lawn += $10
+# candy -= $5
+
+# Week 2: Bonus week and purchase
+# allowance += 60
+# vacuum += $7
+# toy -= $8
+
+# Week 3: Savings
+# Account = 30
+
+# Print final allowance
+# Allowance: $43.0
