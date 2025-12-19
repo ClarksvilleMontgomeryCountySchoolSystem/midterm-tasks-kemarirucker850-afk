c9ff0f7e060c4e96266993181a0936160faa16db
kemarirucker850-afk <kemari.rucker850@students.cmcss.net>
2025-12-19 08:53:06 -0600

    Update task1.py

--- task1.py
+++ task1.py
@@ -1,1 +1,24 @@
 
+# slices = 42
+# print("share 7")
+
+# people = 7
+# share = 4
+# leftover = 0
+# print("share" {0}
+# print("leftover" {4}
+
+# people += 2
+# share = 3
+# leftover = 1
+# print("each person 3")
+# print("leftover slices: 1")
+
+#Mom says "Wait, Brandon’s coming. We’re going to need more pizza. I’ll upgrade the mini to a party_pizza instead. It’s the same as 2 minis. Hopefully the leftovers will be enough to fill his hollow leg.”
+
+# slices = 28
+# share = 14
+# leftover = 6
+# print("share 14")
+# print ("leftover 6")
+print(" 2 for Mr. Hollow Leg")
